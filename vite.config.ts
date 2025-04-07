2aa087d70ce29dc667d4d8c04a6e970ce678d32e
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-04-08 00:11:37 +0200

    feat: (WIP) setup tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import reactStack from "hono-vite-react-stack";
 import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-	plugins: [reactStack()],
+	plugins: [
+		TanStackRouterVite({
+			target: "react",
+			autoCodeSplitting: true,
+			routesDirectory: "./src/client/routes",
+			generatedRouteTree: "./src/client/routeTree.gen.ts",
+		}),
+		reactStack({
+			clientEntry: "./src/client/routes/main.tsx",
+			serverEntry: "src/server/index.tsx",
+			buildOutputDir: "dist-server",
+			cssEntry: "src/style.css",
+			serverDirectories: ["src/server/*"],
+		}),
+	],
 });
