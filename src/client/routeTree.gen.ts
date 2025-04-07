f1b830ac18524d3bb10f67da13d17ecb47fe824d
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-04-08 00:44:52 +0200

    feat: add biome + format code base

--- src/client/routeTree.gen.ts
+++ src/client/routeTree.gen.ts
@@ -10,53 +10,53 @@
 
 // Import Routes
 
-import { Route as rootRoute } from './routes/__root'
-import { Route as MainImport } from './routes/main'
-import { Route as AboutImport } from './routes/about'
-import { Route as IndexImport } from './routes/index'
+import { Route as rootRoute } from "./routes/__root"
+import { Route as MainImport } from "./routes/main"
+import { Route as AboutImport } from "./routes/about"
+import { Route as IndexImport } from "./routes/index"
 
 // Create/Update Routes
 
 const MainRoute = MainImport.update({
-  id: '/main',
-  path: '/main',
+  id: "/main",
+  path: "/main",
   getParentRoute: () => rootRoute,
 } as any)
 
 const AboutRoute = AboutImport.update({
-  id: '/about',
-  path: '/about',
+  id: "/about",
+  path: "/about",
   getParentRoute: () => rootRoute,
 } as any)
 
 const IndexRoute = IndexImport.update({
-  id: '/',
-  path: '/',
+  id: "/",
+  path: "/",
   getParentRoute: () => rootRoute,
 } as any)
 
 // Populate the FileRoutesByPath interface
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
+    "/": {
+      id: "/"
+      path: "/"
+      fullPath: "/"
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
-    '/about': {
-      id: '/about'
-      path: '/about'
-      fullPath: '/about'
+    "/about": {
+      id: "/about"
+      path: "/about"
+      fullPath: "/about"
       preLoaderRoute: typeof AboutImport
       parentRoute: typeof rootRoute
     }
-    '/main': {
-      id: '/main'
-      path: '/main'
-      fullPath: '/main'
+    "/main": {
+      id: "/main"
+      path: "/main"
+      fullPath: "/main"
       preLoaderRoute: typeof MainImport
       parentRoute: typeof rootRoute
     }
@@ -66,30 +66,30 @@ declare module '@tanstack/react-router' {
 // Create and export the route tree
 
 export interface FileRoutesByFullPath {
-  '/': typeof IndexRoute
-  '/about': typeof AboutRoute
-  '/main': typeof MainRoute
+  "/": typeof IndexRoute
+  "/about": typeof AboutRoute
+  "/main": typeof MainRoute
 }
 
 export interface FileRoutesByTo {
-  '/': typeof IndexRoute
-  '/about': typeof AboutRoute
-  '/main': typeof MainRoute
+  "/": typeof IndexRoute
+  "/about": typeof AboutRoute
+  "/main": typeof MainRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
-  '/': typeof IndexRoute
-  '/about': typeof AboutRoute
-  '/main': typeof MainRoute
+  "/": typeof IndexRoute
+  "/about": typeof AboutRoute
+  "/main": typeof MainRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/about' | '/main'
+  fullPaths: "/" | "/about" | "/main"
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/about' | '/main'
-  id: '__root__' | '/' | '/about' | '/main'
+  to: "/" | "/about" | "/main"
+  id: "__root__" | "/" | "/about" | "/main"
   fileRoutesById: FileRoutesById
 }
 
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,16 +1,16 @@
-import { Hono } from "hono";
-import { renderer } from "./renderer";
+import { Hono } from "hono"
+import { renderer } from "./renderer"
 
-const app = new Hono();
+const app = new Hono()
 
 app.get("/api", (c) => {
-	return c.json({ message: "Hello from API" });
-});
+  return c.json({ message: "Hello from API" })
+})
 
-app.use(renderer);
+app.use(renderer)
 
 app.get("/", (c) => {
-	return c.render(<div id="root" />);
-});
+  return c.render(<div id="root"></div>)
+})
 
-export default app;
+export default app
