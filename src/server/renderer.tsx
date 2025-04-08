b89ba03431a745b388bf9bfc85e5e0f11d27f245
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-04-08 09:26:10 +0200

    fix: html loading index.tsx as clientEntry to main.tsx

--- src/server/renderer.tsx
+++ src/server/renderer.tsx
@@ -8,7 +8,7 @@ export const renderer = reactRenderer(({ children }) => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>File Sharing</title>
-        <Script />
+        <Script src="src/client/main.tsx" />
         <Link href="/src/style.css" rel="stylesheet" />
       </head>
       <body>{children}</body>
